0bf2c817d608822b51b50f10ad234561d51105cd
GenieJSexy <samuelbaum26@gmail.com>
2022-09-07 23:38:40 +0300

    modal

--- study js/practic JS/003 Food-dist/js/script.js
+++ study js/practic JS/003 Food-dist/js/script.js
@@ -45,17 +45,12 @@ window.addEventListener("load", () => {
             hours = 0;
             minutes = 0;
             seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24));
+            hours = Math.floor(t / (1000 * 60 * 60) % 60) - 3;
+            minutes = Math.floor(t / (1000 * 60) % 60);
+            seconds = Math.floor((t / 1000) % 60);
         }
-        days = Math.floor(t / (1000 * 60 * 60 * 24));
-        hours = Math.floor(t / (1000 * 60 * 60) % 60) - 3;
-        minutes = Math.floor(t / (1000 * 60) % 60);
-        seconds = Math.floor((t / 1000) % 60);
-
-        console.log(days);
-        console.log(hours);
-        console.log(minutes);
-        console.log(seconds);
-
         return {
             total: t,
             days: days,
@@ -79,6 +74,7 @@ window.addEventListener("load", () => {
             seconds = timer.querySelector("#seconds"),
             timeInterval = setInterval(updateClock, 1000);
         updateClock();
+
         function updateClock() {
             let timer = getTimeRemaining(endtime);
             days.innerHTML = getZero(timer.days);
@@ -91,5 +87,51 @@ window.addEventListener("load", () => {
         }
     }
     setClock(".timer", deadline);
+    // Modal 
 
+    function openModal() {
+        modalMenu.classList.add("show");
+        modalMenu.classList.remove("hide");
+        document.body.style.overflow = "hidden";
+        clearInterval(modalTimerId);
+    }
+    function closeModal() {
+        modalMenu.classList.add("hide");
+        modalMenu.classList.remove("show");
+        document.body.style.overflow = "";
+    }
+
+
+
+    let modalMenu = document.querySelector(".modal");
+    document.querySelectorAll("[data-modal]").forEach(modal => {
+        modal.addEventListener("click", openModal);
+    });
+    document.querySelector("[data-close]").addEventListener("click", () => {
+        // modalMenu.style.display = "none";
+        closeModal();
+
+    });
+    modalMenu.addEventListener("click", (e) => {
+        if (e.target === modalMenu) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener("keydown", (e) => {
+        if (e.code == "Escape" && modalMenu.classList.contains("show")) {
+            closeModal();
+        }
+    });
+
+    let modalTimerId = setTimeout(openModal, 3000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModal();
+            window.removeEventListener("scroll", showModalByScroll);
+        }
+    }
+
+    window.addEventListener("scroll", showModalByScroll);
 });
